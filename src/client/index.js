fc70dc14258268f241b367653ef02ec0195b5eb4
Aaron Falloon <contact@aaronfalloon.com>
2017-01-16 13:06:01 +0000

    Fix typo

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import thunkMiddleware from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
-import { fetchForecast } from '.actions';
+import { fetchForecast } from './actions';
 
 const store = createStore(
   rootReducer,
