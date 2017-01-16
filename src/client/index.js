3869d44f315c3612931b4c2eaca4851802201011
Aaron Falloon <contact@aaronfalloon.com>
2017-01-16 13:05:30 +0000

    Create index.js to tie hook everything together

---
+++ src/client/index.js
@@ -0,0 +1,11 @@
+import thunkMiddleware from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers';
+import { fetchForecast } from '.actions';
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(thunkMiddleware),
+);
+
+store.dispatch(fetchForecast());
